2a935e9b36b7e2f254467798e8c888e4ecc1d81a
ej <277938225@qq.com>
2020-07-07 18:08:24 +0800

    把工程添加到本地仓库

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/GoodsService.java
@@ -0,0 +1,80 @@
+package com.leyou.item.service;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.leyou.common.pojo.PageResult;
+import com.leyou.item.bo.SpuBo;
+import com.leyou.item.mapper.BrandMapper;
+import com.leyou.item.mapper.SpuDetailMapper;
+import com.leyou.item.mapper.SpuMapper;
+import com.leyou.item.pojo.Brand;
+import com.leyou.item.pojo.Spu;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GoodsService {
+
+    @Autowired
+    private SpuMapper spuMapper;
+    @Autowired
+    private SpuDetailMapper spuDetailMapper;
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private BrandMapper brandMapper;
+
+    /**
+     * 根据条件查询分页
+     * @param key
+     * @param saleable
+     * @param page
+     * @param rows
+     * @return
+     */
+    public PageResult<SpuBo> querySpuBoByPage(String key, Boolean saleable, Integer page, Integer rows) {
+        Example example = new Example(Spu.class);
+        Example.Criteria criteria = example.createCriteria();
+        //添加查询条件
+        if (StringUtils.isNotBlank(key)) {
+            criteria.andLike("title", "%" + key + "%");
+        }
+        //添加上下架条件
+        if (saleable != null) {
+            criteria.andEqualTo("saleable", saleable);
+        }
+        //添加分页
+        Page<Object> objects = PageHelper.startPage(page, rows);
+        //执行查询，获取spu集合
+        List<Spu> spus = this.spuMapper.selectByExample(example);
+        PageInfo<spus> pageInfo = new PageInfo<spus>();
+        //spu结合转化
+        List<SpuBo> spubos = spus.stream().map(spu -> {
+            SpuBo spuBo = new SpuBo();
+            // copy共同属性的值到新的对象
+            BeanUtils.copyProperties(spu, spuBo);
+
+            //查询品牌名称
+            Brand brand = this.brandMapper.selectByPrimaryKey(spu.getBrandId());
+            spuBo.setBname(brand.getName());
+            //查询分类名称
+            List<String> names = this.categoryService.queryNamesByIds(Arrays.asList(spu.getCid1(),spu.getCid2(),spu.getCid3()));
+            spuBo.setCname(StringUtils.join(names,"-"));
+            return spuBo;
+        }).collect(Collectors.toList());
+
+
+        //返回PageResult<SpuBo>
+        return new PageResult<>(pageInfo.getTotal(),spubos);
+
+    }
+}
